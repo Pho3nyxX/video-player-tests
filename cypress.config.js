be5c0cc050d0adcd49a7e38780bb05963d9ffe2d
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2025-01-23 19:53:12 -0500

    initial commit

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000/',
+    specPattern: 'cypress/e2e/tests/**/*.cy.{js,jsx,ts,tsx}',
+    defaultCommandTimeout: 15000,
+  },
+});
